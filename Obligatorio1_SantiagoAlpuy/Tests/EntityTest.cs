9acf6ae51e236ddcee39a12694b36f34314469e1
Santiago Alpuy <santiagoalpuysantana@gmail.com>
2020-04-18 20:48:26 -0300

    [RED] - Falla el registro de una entidad.

---
+++ Obligatorio1_SantiagoAlpuy/Tests/EntityTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class EntityTest
+    {
+        [TestMethod]
+        public void RegisterEntity()
+        {
+            EntityTest entity1 = new EntityTest()
+            {
+                Name = "Pepsi",
+            };
+            EntityTest entity2 = new EntityTest()
+            {
+                Name = "Limol",
+            };
+            entity1.AddEntity(entity1);
+            entity1.AddEntity(entity2);
+            Assert.AreEqual(entity1, entity1.ObtainEntity(entity1.Name));
+            Assert.AreEqual(entity2, entity1.ObtainEntity(entity2.Name));
+        }
+    }
+}
