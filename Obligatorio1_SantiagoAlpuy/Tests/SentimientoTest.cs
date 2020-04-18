5d02ea50bc446d5421bbf2ad1eadc5f422ade372
Santiago Alpuy <santiagoalpuysantana@gmail.com>
2020-04-18 20:23:45 -0300

    [RED] - No se pudo ingresar el sentimiento.

--- Obligatorio1_SantiagoAlpuy/Tests/SentimientoTest.cs
+++ Obligatorio1_SantiagoAlpuy/Tests/SentimientoTest.cs
@@ -7,5 +7,17 @@ namespace Tests
     [TestClass]
     public class SentimientoTest
     {
-    }//
+        [TestMethod]
+        public void RegisterSentiment()
+        {
+            Sentiment sentiment = new Sentiment()
+            {
+                Description = "Me gusta",
+                Category = true,
+            };
+            sentiment.AddSentiment(sentiment);
+            sentimentReturned = sentiment.ObtainSentiment(sentiment);
+            Assert.AreEqual(sentiment, sentimentReturned);
+        }
+    }
 }
