a6b4c9777270859f62488c60b41469bbc897ea85
Santiago Alpuy <santiagoalpuysantana@gmail.com>
2020-04-18 20:07:26 -0300

    Inicializacion

---
+++ Obligatorio1_SantiagoAlpuy/Tests/SentimientoTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class SentimientoTest
+    {
+    }//
+}
