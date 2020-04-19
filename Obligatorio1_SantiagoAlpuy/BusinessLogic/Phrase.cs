bd6df959c9a9dd2f1c8b9a8986d176f62d84b6b3
Santiago Alpuy <santiagoalpuysantana@gmail.com>
2020-04-18 21:15:39 -0300

    [GREEN] - Se pudo ingresar una frase.

---
+++ Obligatorio1_SantiagoAlpuy/BusinessLogic/Phrase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusinessLogic
+
+{
+    public class Phrase
+    {
+        public string Comment { get; set; }
+        public string Date { get; set; }
+
+        public List<Phrase> phrase;
+
+        public Phrase()
+        {
+            phrase = new List<Phrase>();
+        }
+
+        public void AddPhrase(Phrase phrase)
+        {
+            this.phrase.Add(phrase);
+        }
+
+        public Phrase ObtainPhrase(string comment, string date)
+        {
+            return phrase.Find(x => x.Comment == comment && x.Date == date);
+        }
+    }
+}
