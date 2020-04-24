e7eef041e3876a4dc5e370fd8252ee78c483dc41
Santiago Alpuy <santiagoalpuysantana@gmail.com>
2020-04-24 03:24:36 -0300

    [INTERFACE] - Quedaron todas las funcionalidades andando. Tengo sueño. Hay que depurar muchas cosas.

--- Obligatorio1_SantiagoAlpuy/UserInterface/UC_AlertConfig.Designer.cs
+++ Obligatorio1_SantiagoAlpuy/UserInterface/UC_AlertConfig.Designer.cs
@@ -28,26 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridPositiveSentiments = new System.Windows.Forms.DataGridView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPositiveSentiments)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleDescription = "";
+            this.textBox1.AccessibleName = "";
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(44, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(526, 35);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "Ingrese el nombre de una entidad";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(364, 198);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(185, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "AlertConfig";
+            this.label1.Size = new System.Drawing.Size(336, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Crear Configuración de Alarma";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Agregar";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::UserInterface.Properties.Resources.icons8_add_36;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 38);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(590, 135);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(141, 38);
+            this.panel1.TabIndex = 13;
+            // 
+            // dataGridPositiveSentiments
+            // 
+            this.dataGridPositiveSentiments.AllowUserToResizeColumns = false;
+            this.dataGridPositiveSentiments.AllowUserToResizeRows = false;
+            this.dataGridPositiveSentiments.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridPositiveSentiments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridPositiveSentiments.Location = new System.Drawing.Point(44, 272);
+            this.dataGridPositiveSentiments.MultiSelect = false;
+            this.dataGridPositiveSentiments.Name = "dataGridPositiveSentiments";
+            this.dataGridPositiveSentiments.ReadOnly = true;
+            this.dataGridPositiveSentiments.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridPositiveSentiments.Size = new System.Drawing.Size(665, 118);
+            this.dataGridPositiveSentiments.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AccessibleDescription = "";
+            this.textBox3.AccessibleName = "";
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Gray;
+            this.textBox3.Location = new System.Drawing.Point(44, 128);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(526, 35);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.Text = "Ingrese la cantidad de posts para activar la alarma";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown);
+            this.textBox3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyUp);
+            // 
+            // textBox4
+            // 
+            this.textBox4.AccessibleDescription = "";
+            this.textBox4.AccessibleName = "";
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.Color.Gray;
+            this.textBox4.Location = new System.Drawing.Point(44, 169);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(526, 35);
+            this.textBox4.TabIndex = 16;
+            this.textBox4.Text = "Ingrese el rango de días para activar la alarma";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyDown);
+            this.textBox4.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox4_KeyUp);
+            // 
+            // textBox5
+            // 
+            this.textBox5.AccessibleDescription = "";
+            this.textBox5.AccessibleName = "";
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.ForeColor = System.Drawing.Color.Gray;
+            this.textBox5.Location = new System.Drawing.Point(44, 210);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(526, 35);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.Text = "Ingrese el rango de horas para activar la alarma";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox5_KeyDown);
+            this.textBox5.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox5_KeyUp);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(44, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(526, 21);
+            this.comboBox1.TabIndex = 18;
             // 
             // UC_AlertConfig
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::UserInterface.Properties.Resources.unnamed;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridPositiveSentiments);
             this.Name = "UC_AlertConfig";
             this.Size = new System.Drawing.Size(762, 408);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPositiveSentiments)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,6 +205,15 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridPositiveSentiments;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Obligatorio1_SantiagoAlpuy/UserInterface/UC_AlertConfig.cs
+++ Obligatorio1_SantiagoAlpuy/UserInterface/UC_AlertConfig.cs
@@ -7,14 +7,172 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogic;
+using BusinessLogic.Controllers;
+using BusinessLogic.Exceptions;
 
 namespace UserInterface
 {
     public partial class UC_AlertConfig : UserControl
     {
+        Repository repository;
+        AlertController alertController;
+        private const string WRITE_ENTITY_MESSAGE = "Ingrese el nombre de una entidad";
+        private const string WRITE_POST_COUNT_MESSAGE = "Ingrese la cantidad de posts para activar la alarma";
+        private const string WRITE_DAY_COUNT_MESSAGE = "Ingrese el rango de días para activar la alarma";
+        private const string WRITE_HOUR_COUNT_MESSAGE = "Ingrese el rango de horas para activar la alarma";
+        private const string FIELDS_NOT_ADDED = "Debes completar TODOS los campos.";
+        private const string ALERT_ADDED_SUCCESFULLY = "Alerta agregada satisfactoriamente.";
+        private const string NEGATIVE_POST_COUNT = "No puedes ingresar cantidad de posts negativos.";
+        private const string NEGATIVE_DAY_COUNT = "No puedes ingresar cantidad de posts negativos.";
+        private const string NEGATIVE_HOUR_COUNT = "No puedes ingresar cantidad de posts negativos.";
+
+
         public UC_AlertConfig()
         {
             InitializeComponent();
+            repository = Repository.Instance;
+            alertController = new AlertController();
+            foreach (CategoryType item in Enum.GetValues(typeof(CategoryType)))
+            {
+                if (!item.Equals(CategoryType.Neutro))
+                    comboBox1.Items.Add(item);
+            }
+            LoadDataGridAlerts();
+        }
+
+        private void LoadDataGridAlerts()
+        {
+            this.dataGridPositiveSentiments.DataSource = repository.alerts.ToList();
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = WRITE_ENTITY_MESSAGE;
+                textBox1.ForeColor = Color.Gray;
+            }
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (textBox1.Text == WRITE_ENTITY_MESSAGE)
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (textBox3.Text == WRITE_POST_COUNT_MESSAGE)
+            {
+                textBox3.Text = "";
+                textBox3.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox3_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (textBox3.Text == "")
+            {
+                textBox3.Text = WRITE_POST_COUNT_MESSAGE;
+                textBox3.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (textBox4.Text == WRITE_DAY_COUNT_MESSAGE)
+            {
+                textBox4.Text = "";
+                textBox4.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox4_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (textBox4.Text == "")
+            {
+                textBox4.Text = WRITE_DAY_COUNT_MESSAGE;
+                textBox4.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBox5_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (textBox5.Text == WRITE_HOUR_COUNT_MESSAGE)
+            {
+                textBox5.Text = "";
+                textBox5.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox5_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (textBox5.Text == "")
+            {
+                textBox5.Text = WRITE_HOUR_COUNT_MESSAGE;
+                textBox5.ForeColor = Color.Gray;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != WRITE_ENTITY_MESSAGE && textBox3.Text != WRITE_POST_COUNT_MESSAGE && textBox4.Text != WRITE_DAY_COUNT_MESSAGE && 
+                textBox5.Text != WRITE_HOUR_COUNT_MESSAGE)
+            {
+                CreateAndAddAlarm();
+
+            }
+            else
+            {
+                MessageBox.Show(FIELDS_NOT_ADDED);
+            }
+        }
+
+        private void CreateAndAddAlarm()
+        {
+            try
+            {
+                Alert alert = new Alert()
+                {
+                    Entity = textBox1.Text,
+                    Category = CategoryType.Positive,
+                    Posts = Int32.Parse(textBox3.Text),
+                    Days = Int32.Parse(textBox4.Text),
+                    Hours = Int32.Parse(textBox5.Text)
+                };
+                alertController.AddAlert(alert);
+                MessageBox.Show(ALERT_ADDED_SUCCESFULLY);
+                textBox1.Text = WRITE_ENTITY_MESSAGE;
+                textBox1.ForeColor = Color.Gray;
+                textBox3.Text = WRITE_POST_COUNT_MESSAGE;
+                textBox3.ForeColor = Color.Gray;
+                textBox4.Text = WRITE_DAY_COUNT_MESSAGE;
+                textBox4.ForeColor = Color.Gray;
+                textBox5.Text = WRITE_HOUR_COUNT_MESSAGE;
+                textBox5.ForeColor = Color.Gray;
+                comboBox1.Text = "";
+
+
+                LoadDataGridAlerts();
+            }
+            catch (NegativePostCountException e)
+            {
+                MessageBox.Show(NEGATIVE_POST_COUNT);
+            }
+            catch (NegativeDayException e)
+            {
+                MessageBox.Show(NEGATIVE_DAY_COUNT);
+            }
+            catch (NegativeHourException e)
+            {
+                MessageBox.Show(NEGATIVE_HOUR_COUNT);
+            }
         }
     }
 }
