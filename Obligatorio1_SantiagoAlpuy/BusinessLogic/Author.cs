6ab5be2bcd6d63d084578f0e4e58d535d2b18c54
Santiago Alpuy <santiagoalpuysantana@gmail.com>
2020-05-27 00:39:19 -0300

    [GREEN] - Paso el test de agregar dos autores a una lista en la clase AuthorTest

---
+++ Obligatorio1_SantiagoAlpuy/BusinessLogic/Author.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class Author
+    {
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Born { get; set; }
+    }
+}
--- Obligatorio1_SantiagoAlpuy/Tests/AuthorTest.cs
+++ Obligatorio1_SantiagoAlpuy/Tests/AuthorTest.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using System.Collections.Generic;
 
 namespace Tests
 {
     [TestClass]
     public class AuthorTest
     {
+        List<Author> authors = new List<Author>();
         [TestMethod]
-        public void RegisterAuthor()
+        public void RegisterAuthors()
         {
-            Author author = new Author() { Username = "testUser", Name = "name1", Surname= "surname1", Born=DateTime.Now};
+            Author author1 = new Author() { Username = "testUser", Name = "name1", Surname= "surname1", Born=DateTime.Now};
+            Author author2 = new Author() { Username = "testUser", Name = "name1", Surname = "surname1", Born = DateTime.Now };
+            authors.Add(author1);
+            authors.Add(author2);
+            Assert.IsTrue(authors.Contains(author1));
+            Assert.IsTrue(authors.Contains(author2));
         }
     }
 }
