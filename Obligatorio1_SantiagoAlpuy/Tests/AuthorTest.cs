a68f44c5de97562d8efb442ade35974768db1756
Santiago Alpuy <santiagoalpuysantana@gmail.com>
2020-05-27 00:26:44 -0300

    [RED] - No paso el test de registrar un autor

---
+++ Obligatorio1_SantiagoAlpuy/Tests/AuthorTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class AuthorTest
+    {
+        [TestMethod]
+        public void RegisterAuthor()
+        {
+            Author author = new Author() { Username = "testUser", Name = "name1", Surname= "surname1", Born=DateTime.Now};
+        }
+    }
+}
