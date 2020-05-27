d48cd9b86ac90e61619b797fefc6989c5bf58b52
Santiago Alpuy <santiagoalpuysantana@gmail.com>
2020-05-27 02:05:14 -0300

    [GREEN] - Se lanzo una excepción cuando se intento agregar un usuario con caracteres no alfanumericos

--- Obligatorio1_SantiagoAlpuy/BusinessLogic/Controllers/AuthorController.cs
+++ Obligatorio1_SantiagoAlpuy/BusinessLogic/Controllers/AuthorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using BusinessLogic.Exceptions;
 using BusinessLogic.IControllers;
+using System.Text.RegularExpressions;
 
 namespace BusinessLogic.Controllers
 {
@@ -12,6 +13,7 @@ namespace BusinessLogic.Controllers
         private List<Author> authors;
         private const int MAX_CHARS_IN_USERNAME = 10;
         private const string USERNAME_IS_TOO_BIG = "El nombre de usuario es mayor a {MAX_CHARS_IN_USERNAME} caracteres.";
+        private const string USERNAME_IS_NOT_ALPHANUMERIC = "El nombre de usuario contiene caracteres no alfanumericos.";
 
         public AuthorController()
         {
@@ -24,8 +26,17 @@ namespace BusinessLogic.Controllers
                 throw new LackOfObligatoryParametersException();
             else if (author.Username.Length >= MAX_CHARS_IN_USERNAME)
                 throw new TooLargeException(USERNAME_IS_TOO_BIG);
+            else if (!IsAlphanumeric(author.Username))
+                throw new NotAlphaNumericalException(USERNAME_IS_NOT_ALPHANUMERIC);
             else
                 authors.Add(author);
+        }
+
+        private bool IsAlphanumeric(string text)
+        {
+            Regex r = new Regex("^[a-zA-Z0-9]*$");
+            return r.IsMatch(text);
+            
         }
 
         public Author ObtainAuthorByUsername(string username)
