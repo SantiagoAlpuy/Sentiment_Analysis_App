3ef1b989aa8002c2387f441ad40e75eab7d1de35
Santiago Alpuy <santiagoalpuysantana@gmail.com>
2020-05-31 16:18:31 -0300

    [RED] - No se lanzo una excepción cuando se intento modificar el nombre de un usuario, por un nombre con muchos espacios en blanco.

--- Obligatorio1_SantiagoAlpuy/Tests/AuthorTest.cs
+++ Obligatorio1_SantiagoAlpuy/Tests/AuthorTest.cs
@@ -222,5 +222,16 @@ namespace Tests
             authorController.ModifyAuthor(author1, author2);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ModifyAuthorNameToNameWithOnlyBlankSpaces()
+        {
+            Author author1 = new Author() { Username = "testUserA", Name = "nameA", Surname = "surnameA", Born = new DateTime(1980, 01, 01) };
+            Author author2 = new Author() { Username = "testUserA", Name = "    ", Surname = "surnameA", Born = new DateTime(1980, 01, 01) };
+            authorController.ModifyAuthor(author1, author2);
+        }
+
+
+
     }
 }
