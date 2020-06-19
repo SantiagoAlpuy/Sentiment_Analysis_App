618103418a9503c098b067e2b1345c75a81f4fbb
= <=>
2020-06-18 23:34:56 -0300

    [REFACTOR] - Se arreglaron las relaciones One-To-Many que configuran la relación Many-To-Many

--- Obligatorio1_SantiagoAlpuy/BusinessLogic/AuthorConfiguration.cs
+++ Obligatorio1_SantiagoAlpuy/BusinessLogic/AuthorConfiguration.cs
@@ -8,10 +8,6 @@ namespace BusinessLogic
         public AuthorConfiguration()
         {
             this.HasMany<Phrase>(x => x.Phrases).WithRequired(y => y.Author).WillCascadeOnDelete();
-
-            this.HasMany(x => x.AlertBAuthors)
-            .WithRequired()
-            .HasForeignKey(x => x.AuthorId);
         }
     }
 }
